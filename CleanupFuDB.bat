9aa381dec8084e10ccaa037f587fd79353a5f5a2
Ciaran Greene <ciarangreene84@users.noreply.github.com>
2019-04-23 13:18:41 +0200

    Update CleanupFuDB.bat

--- CleanupFuDB.bat
+++ CleanupFuDB.bat
@@ -9,42 +9,6 @@ sqlcmd -S localhost\ -d WebERP -i WebERP_DropTablesScript.sql
 DEL WebERP_DropTablesScript.sql
 sqlcmd -S localhost\ -d WebERP -i 998_DropStoredProcedures.sql
 
-@ECHO 'DeFuDBing WebERP_Archive'
-sqlcmd -S localhost\ -d WebERP_Archive -i 996_DropTriggersScript.sql -o WebERP_Archive_DropTriggersScript.sql -f 1255
-sqlcmd -S localhost\ -d WebERP_Archive -i WebERP_Archive_DropTriggersScript.sql
-DEL WebERP_Archive_DropTriggersScript.sql
-sqlcmd -S localhost\ -d WebERP_Archive -i 997_DropTablesScript.sql -o WebERP_Archive_DropTablesScript.sql -f 1255
-sqlcmd -S localhost\ -d WebERP_Archive -i WebERP_Archive_DropTablesScript.sql
-DEL WebERP_Archive_DropTablesScript.sql
-sqlcmd -S localhost\ -d WebERP_Archive -i 998_DropStoredProcedures.sql
-
-@ECHO 'DeFuDBing WebERP_Claims'
-sqlcmd -S localhost\ -d WebERP_Claims -i 996_DropTriggersScript.sql -o WebERP_Claims_DropTriggersScript.sql -f 1255
-sqlcmd -S localhost\ -d WebERP_Claims -i WebERP_Claims_DropTriggersScript.sql
-DEL WebERP_Claims_DropTriggersScript.sql
-sqlcmd -S localhost\ -d WebERP_Claims -i 997_DropTablesScript.sql -o WebERP_Claims_DropTablesScript.sql -f 1255
-sqlcmd -S localhost\ -d WebERP_Claims -i WebERP_Claims_DropTablesScript.sql
-DEL WebERP_Claims_DropTablesScript.sql
-sqlcmd -S localhost\ -d WebERP_Claims -i 998_DropStoredProcedures.sql
-
-@ECHO 'DeFuDBing WebERP_Finance'
-sqlcmd -S localhost\ -d WebERP_Finance -i 996_DropTriggersScript.sql -o WebERP_Finance_DropTriggersScript.sql -f 1255
-sqlcmd -S localhost\ -d WebERP_Finance -i WebERP_Finance_DropTriggersScript.sql
-DEL WebERP_Finance_DropTriggersScript.sql
-sqlcmd -S localhost\ -d WebERP_Finance -i 997_DropTablesScript.sql -o WebERP_Finance_DropTablesScript.sql -f 1255
-sqlcmd -S localhost\ -d WebERP_Finance -i WebERP_Finance_DropTablesScript.
-DEL WebERP_Finance_DropTablesScript.sql
-sqlcmd -S localhost\ -d WebERP_Finance -i 998_DropStoredProcedures.sql
-
-@ECHO 'DeFuDBing WebERP_SystemLogs'
-sqlcmd -S localhost\ -d WebERP_SystemLogs -i 996_DropTriggersScript.sql -o WebERP_SystemLogs_DropTriggersScript.sql -f 1255
-sqlcmd -S localhost\ -d WebERP_SystemLogs -i WebERP_SystemLogs_DropTriggersScript.sql
-DEL WebERP_SystemLogs_DropTriggersScript.sql
-sqlcmd -S localhost\ -d WebERP_SystemLogs -i 997_DropTablesScript.sql -o WebERP_SystemLogs_DropTablesScript.sql -f 1255
-sqlcmd -S localhost\ -d WebERP_SystemLogs -i WebERP_SystemLogs_DropTablesScript.sql
-DEL WebERP_SystemLogs_DropTablesScript.sql
-sqlcmd -S localhost\ -d WebERP_SystemLogs -i 998_DropStoredProcedures.sql
-
 sqlcmd -S localhost\SQLExpress -i ./999_DropInfrastructureScript.sql -o DropInfrastructure.sql -f 1255
 sqlcmd -S localhost\SQLExpress -i DropInfrastructure.sql
 DEL DropInfrastructure.sql
